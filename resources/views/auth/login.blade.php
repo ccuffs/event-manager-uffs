1489d07ee1f899ed156414adf45329bfb082186c
arufonsekun <junior.ramisch@gmail.com>
2020-02-06 01:56:51 -0300

    Implements login layout, this view is quite responsive
    The form.scss must be refactored/improved

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,23 +4,21 @@
 
     <div class="container">
         <div class=row>
-            <div class="col col-xs-12 col-sm-12 col-md-12 col-xl-12">
-                <div class="card">
-                    <div class="card-header">
-                        This is a card-header
-                    </div>
+            <div class="col col-xs-12 col-sm-12 col-md-7 col-lg-7 col-xl-5 mx-auto">
+                <div class="card mt-5">
                     <div class="card-body">
-                        Lorem ipsum dolor sit amet consectetur,
-                        adipisicing elit. Maxime, quidem error
-                        delectus numquam soluta consectetur
-                        veniam iste ad tenetur ut officiis
-                        magnam similique nemo voluptate
-                        ratione exercitationem
-                        reprehenderit enim.
-                        Cupiditate!
-                    </div>
-                    <div class="card-footer">
-                        This is Sparta.
+                        <div class="media text-center">
+                            <img src="{{ asset('/img/logo_uffs.png') }}" class="img-fluid" alt="logo_uffs">
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="IdUFFS">
+                        </div>
+                        <div class="input-group mt-4">
+                            <input type="password" class="form-control" placeholder="Senha">
+                        </div>
+                        <button type="button" class="btn mt-4 mb-3">ENTRAR</button>
+                        <a href="https://id.uffs.edu.br/id/XUI/?realm=/#forgotUsername/" target="_blank" class="text-success mt-2"> Não sabe seu IdUFFS? </a>
+                        <a href="#" target="_blank" class="text-success mx-2"> Esqueci minha senha? </a>
                     </div>
                 </div>
             </div>
