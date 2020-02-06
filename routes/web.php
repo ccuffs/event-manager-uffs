79947ec5b5e3b81b8f5b5b37d8a0a3e768d06b08
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-02-06 19:06:59 -0300

    adds home page adds header (navbar as a header) and footer adds box-shadow variables for scss

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/', 'HomeController@index')->name('home');
