e26c8e109621de94052192399f9ceb82796fd2d4
arufonsekun <junior.ramisch@gmail.com>
2020-02-06 23:21:50 -0300

    refactoring form.scss

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,24 +5,29 @@
     <div class="container">
         <div class=row>
             <div class="col col-xs-12 col-sm-12 col-md-7 col-lg-7 col-xl-5 mx-auto">
-                <div class="card mt-5">
+                <div class="card mt-5 card__shadow">
                     <div class="card-body">
+                        
                         <div class="media text-center">
                             <img src="{{ asset('/img/logo_uffs.png') }}" class="img-fluid" alt="logo_uffs">
                         </div>
+
                         <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="IdUFFS">
+                            <input type="text" class="form-control card__input" placeholder="IdUFFS">
                         </div>
+                        
                         <div class="input-group mt-4">
-                            <input type="password" class="form-control" placeholder="Senha">
+                            <input type="password" class="form-control card__input" placeholder="Senha">
                         </div>
-                        <button type="button" class="btn mt-4 mb-3">ENTRAR</button>
-                        <a href="https://id.uffs.edu.br/id/XUI/?realm=/#forgotUsername/" target="_blank" class="text-success mt-2"> Não sabe seu IdUFFS? </a>
-                        <a href="#" target="_blank" class="text-success mx-2"> Esqueci minha senha? </a>
+                        
+                        <button type="button" class="btn mt-4 mb-3 card__btn">ENTRAR</button>
+                        
+                        <a href="https://id.uffs.edu.br/id/XUI/?realm=/#forgotUsername/" target="_blank" class="text-success mt-2 card__action"> Não sabe seu IdUFFS? </a>
+                        
+                        <a href="#" target="_blank" class="text-success mx-2 card__action"> Esqueceu seu senha? </a>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 
