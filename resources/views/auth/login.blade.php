6707f3589f8f25391ae6f7813e77d4658ecd9feb
arufonsekun <junior.ramisch@gmail.com>
2020-02-14 20:48:37 -0300

    implements @mateusKoppe requiremets from PR 14
    removes useless bootstrap grid breakpoints,
    uses semantic flexbox content centering
    property, extends classes styles in sccss
    instead using them in html elements

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 
     <div class="container">
-        <div class=row>
-            <div class="col col-xs-12 col-sm-12 col-md-7 col-lg-7 col-xl-5 mx-auto">
-                <div class="card mt-5 card__shadow">
+        <div class="row justify-content-center">
+            <div class="col col-xs-12 col-md-7 col-xl-5">
+                <div class="card__shadow">
                     <div class="card-body">
                         
                         <div class="media">
@@ -13,18 +13,19 @@
                         </div>
 
                         <div class="input-group mb-3">
-                            <input type="text" class="form-control card__input" placeholder="IdUFFS">
+                            <input type="text" class="card__input" placeholder="IdUFFS">
                         </div>
                         
                         <div class="input-group mt-4">
-                            <input type="password" class="form-control card__input" placeholder="Senha">
+                            <input type="password" class="card__input" placeholder="Senha">
                         </div>
                         
                         <button type="button" class="btn mt-4 mb-3 card__btn">ENTRAR</button>
                         
-                        <a href="https://id.uffs.edu.br/id/XUI/?realm=/#forgotUsername/" target="_blank" class="text-success mt-2 card__action"> Não sabe seu IdUFFS? </a>
+                        <a href="https://id.uffs.edu.br/id/XUI/?realm=/#forgotUsername/" target="_blank" class="card__action mt-2"> Não sabe seu IdUFFS? </a>
                         
-                        <a href="#" target="_blank" class="text-success mx-2 card__action"> Esqueceu seu senha? </a>
+                        <a href="#" target="_blank" class="card__action mx-2"> Esqueceu seu senha? </a>
+                        
                     </div>
                 </div>
             </div>
