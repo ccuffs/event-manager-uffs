e8a03688b1ad2ec3268c688989dda8b35727411a
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-02-14 19:07:56 -0300

    updates with upstream/master

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,15 +5,15 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="mx-auto order-0">
-                <p class="header__title">Event Manager UFFS</p>
+                <p class="navbar__title">Event Manager UFFS</p>
             </div>
 
             <div class="order-3">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headerUserSection" aria-controls="headerUserSection" aria-expanded="false">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-user-section" aria-controls="navbar-user-section" aria-expanded="false">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="headerUserSection">
+                <div class="collapse navbar-collapse" id="navbar-user-section">
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link" href="#">Sair</a>
@@ -25,38 +25,28 @@
     </header>
 
     {{-- Main --}}
-    <main class="main">
-        <base-card
-            action-card
-        ></base-card>
-
-        <base-card
-            title="Nome do Evento"
-            date="21/04/2020"
-            caard-image="http://uffs.cc/sacc/img/banner.jpg"  
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        ></base-card>
-
-
+    <main class="app-main">
+        <base-card>
+        </base-card>
     </main>
 
     {{-- Footer --}}
-    <footer class="footer">
+    <footer class="app-footer">
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="footer__logo">
+                    <div class="app-footer__logo">
                         Desenvolvido pela FronteiraTec
                     </div>
                 </div>
 
                 <div class="col-md-6">
-                    <div class="footer__nav">
-                        <div class="footer__nav-title">
+                    <div class="app-footer__nav">
+                        <div class="app-footer__nav-title">
                             Links úteis
                         </div>
 
-                        <a href="http://cc.uffs.edu.br/" class="footer__link">CC UFFS</a>
+                        <a href="http://cc.uffs.edu.br/" class="app-footer__link">CC UFFS</a>
                     </div>
                 </div>
 
