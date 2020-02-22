897369e52d52f3195bec655c835af02d76e4c36a
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-02-22 14:46:00 -0300

    adds a modal with form for event creation

--- resources/js/components/EventForm.vue
+++ resources/js/components/EventForm.vue
@@ -1,29 +1,53 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 offset-md-1 col-12">
-                <form class="">
-                    <div class="form-group">
-                        <label for="eventName">Nome do Evento</label>
-                        <input type="text" id="eventName" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="eventDescription">Breve Resumo</label>
-                        <input type="text" id="eventDescription" class="form-control">
-                    </div>
-
-                    <a href="#" class="btn btn-primary">
-                        Salvar
-                    </a>
-                </form>
-            </div>
+    <base-modal
+        modalid="createEventModal"
+        variant="dark"
+        @create="createEvent"
+    >
+        <div class="form-group">
+            <label for="eventName">Nome do evento</label>
+            <input type="text" name="eventName" id="eventName" class="form-control">
         </div>
-    </div>
+
+        <div class="form-group">
+            <label for="eventPlace">Local do evento</label>
+            <input type="text" name="eventPlace" id="eventPlace" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="eventDescription">Breve descrição</label>
+            <textarea name="eventDescription" id="eventDescription" class="form-control" rows="3"></textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="eventDateStart">Data de início do evento</label>
+            <input type="date" name="eventDateStart" id="eventDateStart" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="eventDateEnd">Data de térmimo do evento</label>
+            <input type="date" name="eventDateEnd" id="eventDateEnd" class="form-control">
+        </div>
+    </base-modal>
 </template>
 
 <script>
 export default {
+    methods: {
+        async createEvent() {
+            let data = {
+                'event_name': document.getElementById('eventName').value,
+                'place': document.getElementById('eventPlace').value,
+                'description': document.getElementById('eventDescription').value,
+                'dateStart': document.getElementById('eventDateStart').value,
+                'dateEnd': document.getElementById('eventDateEnd').value,
+            }
+
+            // await window.axios.post('/api/event', data);
+
+            window.location.replace('/editor');
+        }
+    }
 }
 </script>
 
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,8 @@ Route::get('/', function() {
     return  view('home');
 });
 
+Route::get('/editor', function() {
+    return view('editor');
+});
+
 // Route::get('/', 'HomeController@index')->name('home');
