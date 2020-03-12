bf15ab36866f9a76eb8eb9d30c6c7a98aa98b282
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-03-11 23:00:27 -0300

    cleans home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,42 +8,7 @@
     {{-- Main --}}
     <main class="app-main">
 
-        <base-card
-            action-card
-        >
-        </base-card>
-
-        <base-card
-            title="Lorem Ipsum"
-            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            date="19/03/2020"
-        >
-        </base-card>
-
-        <base-card
-            paper-card
-            variant="dark"
-            padding="large"
-        >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </base-card>
-
-        <base-card
-            paper-card
-            variant="primary"
-            shadow="large"
-        >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </base-card>
-
-        <base-card
-            paper-card
-            variant="light"
-            padding="x-large"
-            shadow="medium"
-        >
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </base-card>
+        
     </main>
 
     {{-- Footer --}}
